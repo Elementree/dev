9df931831229e615d3a4a8fb6460b72debac8aba
Christiaan Lombard <base1.christiaan@gmail.com>
2017-01-30 13:52:17 +0200

    first

---
+++ README.md
@@ -0,0 +1,20 @@
+# DEV
+
+My personal development environment helpers.
+
+
+# Features
+
+  - Setup directory structure
+  - Install portable executables
+    - Heidi
+    - Keypass
+    - ConEmu
+  - Password, SSH key manager?
+  - Provision user files
+      - .homestead
+      - .bash_profile
+      - .vagrant
+      - .gitprofile
+  - Install global packages
+      - atom: apm packages, user settings
---
+++ index.js
@@ -0,0 +1,66 @@
+#!/usr/bin/env node
+
+const path = require('path');
+const fs = require('fs');
+var chalk = require('chalk');
+var cmd = require('commander');
+var co = require('co');
+var prompt = require('prompt');
+
+
+prompt.message = 'dev';
+
+exports.paths = {
+    home: process.env.HOME,
+    dev: __dirname,
+    dev_home: path.join( __dirname, '/home' ),
+    dev_homestead: path.join( __dirname, '/homestead' ),
+    cwd: process.cwd()
+};
+
+
+exports.compareFiles = function (file1, file2){
+    var stat1 = fs.statSync( file1 );
+    var stat2 = fs.statSync( file2 );
+
+    console.log(stat1);
+
+};
+
+
+exports.promptConfirm = function(message, callback){
+    prompt.start();
+
+    var property = {
+        name: 'yesno',
+        message: message,
+        validator: /y[es]*|n[o]?/,
+        warning: 'Must respond yes or no',
+        default: 'no'
+    };
+
+    prompt.get(property, function(err, result){
+        callback(result.yesno === 'yes' || result.yesno === 'y');
+    });
+};
+
+exports.console = {
+    info: function(obj){
+        console.log( chalk.blue(obj) );
+    },
+    file: function(content){
+        console.log( chalk.dim(content) );
+    },
+    error: function(obj){
+        console.log( chalk.blue(obj) );
+    },
+    warn: function(obj){
+        console.log( chalk.yellow(obj) );
+    },
+    success: function(obj){
+        console.log( chalk.green(obj) );
+    },
+    h1: function(obj){
+        console.log(chalk.magenta.bold.underline(obj));
+    },
+};
